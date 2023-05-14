8ab2a5ed6d1a73923c95430ec9cf917382537dc2
matt-coops <matt.cooper.85@gmail.com>
2023-05-14 10:22:57 +1000

    Updated code

--- javascript.js
+++ javascript.js
@@ -12,16 +12,45 @@ const fieldAuthor = document.querySelector("#author");
 const fieldPages = document.querySelector("#pages");
 const checkboxRead = document.querySelector("#read");
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  this.info = function () {
-    return `${title} by ${author}, ${pages} pages, ${
-      read === true ? "has been read" : "not read yet"
-    }`;
-  };
+btnNewBook.addEventListener("click", function (e) {
+  overlayForm.classList.remove("hidden");
+});
+
+btnOverlaySubmit.addEventListener("click", addBookToLibrary);
+
+btnOverlayClose.addEventListener("click", closeForm());
+
+containerBooks.addEventListener("click", function (e) {
+  if (
+    !e.target.classList.contains("delete-book") &&
+    !e.target.classList.contains("book-read-checkbox")
+  )
+    return;
+
+  const index = e.target.closest(".book").dataset.index;
+  if (e.target.classList.contains("delete-book")) {
+    myLibrary.splice(index, 1);
+    renderBook();
+  }
+  if (e.target.classList.contains("book-read-checkbox")) {
+    myLibrary[index].read === true
+      ? (myLibrary[index].read = false)
+      : (myLibrary[index].read = true);
+  }
+});
+
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function () {
+      return `${this.title} by ${this.author}, ${this.pages} pages, ${
+        this.read === true ? "has been read" : "not read yet"
+      }`;
+    };
+  }
 }
 
 function resetForm() {
@@ -32,6 +61,19 @@ function resetForm() {
 }
 
 function addBookToLibrary() {
+  myLibrary.push(
+    new Book(
+      fieldTitle.value,
+      fieldAuthor.value,
+      fieldPages.value,
+      checkboxRead.checked
+    )
+  );
+  resetForm();
+  renderBook();
+}
+
+function renderBook() {
   containerBooks.innerHTML = "";
   for (const [index, book] of myLibrary.entries()) {
     const html = `
@@ -53,47 +95,7 @@ function addBookToLibrary() {
   }
 }
 
-btnNewBook.addEventListener("click", function (e) {
-  overlayForm.classList.remove("hidden");
-});
-
-btnOverlaySubmit.addEventListener("click", function () {
-  myLibrary.push(
-    new Book(
-      fieldTitle.value,
-      fieldAuthor.value,
-      fieldPages.value,
-      checkboxRead.checked
-    )
-  );
-  resetForm();
-  addBookToLibrary();
-});
-
-btnOverlayClose.addEventListener("click", function () {
-  closeForm();
-});
-
-const closeForm = function () {
+function closeForm() {
   resetForm();
   overlayForm.classList.add("hidden");
-};
-
-containerBooks.addEventListener("click", function (e) {
-  if (
-    !e.target.classList.contains("delete-book") &&
-    !e.target.classList.contains("book-read-checkbox")
-  )
-    return;
-
-  const index = e.target.closest(".book").dataset.index;
-  if (e.target.classList.contains("delete-book")) {
-    myLibrary.splice(index, 1);
-    addBookToLibrary();
-  }
-  if (e.target.classList.contains("book-read-checkbox")) {
-    myLibrary[index].read === true
-      ? (myLibrary[index].read = false)
-      : (myLibrary[index].read = true);
-  }
-});
+}
